5bbcfb6ba72f3c1892f0d158e2e721a923a963c2
YoriB <deyoribad@yahoo.co.uk>
2023-01-19 14:19:29 +0000

    nc news8

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -233,19 +233,19 @@ test('should return a status of 400 for a missing username',() => {
 })
   
 
-  describe.only('PATCH  /api/articles/:article_id ', () => {
-    test('should return a status :200 when a valid comment is updated.', () => {
-      const newVote = 1
-      const articleVoted = {inc_votes : newVote}
-      return request(app)
-      .patch(" /api/articles/1")
-      .send(articleVoted)
-      .expect(200)
-      .then(({body}) => {
-        return body;
-        })
-})
-  })
+//   describe.only('PATCH  /api/articles/:article_id ', () => {
+//     test('should return a status :200 when a valid comment is updated.', () => {
+//       const newVote = 1
+//       const articleVoted = {inc_votes : newVote}
+//       return request(app)
+//       .patch(" /api/articles/1")
+//       .send(articleVoted)
+//       .expect(200)
+//       .then(({body}) => {
+//         return body;
+//         })
+// })
+//   })
 })
 })
 
