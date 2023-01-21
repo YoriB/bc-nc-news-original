cd1a5e92678f9d043be2c7440b199c9c74ec25dd
YoriB <deyoribad@yahoo.co.uk>
2023-01-21 10:14:22 +0000

    nc news10

--- models/model.js
+++ models/model.js
@@ -3,27 +3,33 @@ const format = require('pg-format');
 const topics = require('../db/data/development-data/topics');
 const testData = require('../db/data/test-data/index');
 
-
-
-
 const fetchTopics = () => {
   return db.query(`SELECT * FROM topics`).then((result) => {
     return result.rows;
   });
 };
 
-const fetchArticles = (sort_by = 'created_at' , order = 'DESC', topic) => {
+const fetchArticles = (sort_by = 'created_at', order = 'DESC', topic) => {
   const queryValues = [];
-  const acceptedSortBy = ['article_id', 'article_img_url','created_at', 'title', 'topic', 'author', 'votes', 'body' ];
-  const acceptedOrders = ['ASC','DESC'];
+  const acceptedSortBy = [
+    'article_id',
+    'article_img_url',
+    'created_at',
+    'title',
+    'topic',
+    'author',
+    'votes',
+    'body',
+  ];
+  const acceptedOrders = ['ASC', 'DESC'];
 
-  if(!acceptedOrders.includes(order)){
-  return Promise.reject({status: 400, message: 'Bad request'})
-}
-if (!acceptedSortBy.includes(sort_by)) {
-  return Promise.reject({status: 400, message: 'Bad request'})
-}
- 
+  if (!acceptedOrders.includes(order)) {
+    return Promise.reject({ status: 400, message: 'Bad request' });
+  }
+  if (!acceptedSortBy.includes(sort_by)) {
+    return Promise.reject({ status: 400, message: 'Bad request' });
+  }
+
   let queryStr = `SELECT
    articles.article_id, 
    articles.title,
@@ -36,18 +42,17 @@ if (!acceptedSortBy.includes(sort_by)) {
   COUNT(comments.body) AS comment_count
   FROM articles
   LEFT JOIN comments 
-  ON articles.article_id = comments.article_id`
+  ON articles.article_id = comments.article_id`;
 
-if (topic){
-  queryStr += ` WHERE topic = $1`
-  queryValues.push(topic)
-}
-queryStr += ` GROUP BY articles.article_id  ORDER BY ${sort_by} ${order}`
+  if (topic) {
+    queryStr += ` WHERE topic = $1`;
+    queryValues.push(topic);
+  }
+  queryStr += ` GROUP BY articles.article_id  ORDER BY ${sort_by} ${order}`;
 
   return db.query(queryStr, queryValues).then((results) => {
-  
-  return results.rows
-})  
+    return results.rows;
+  });
 };
 
 const fetchArticlesById = (article_id) => {
@@ -56,74 +61,73 @@ const fetchArticlesById = (article_id) => {
   }
 
   return db
-    .query(`SELECT * 
+    .query(
+      `SELECT * 
     FROM articles 
-    WHERE article_id = $1`, [article_id])
+    WHERE article_id = $1`,
+      [article_id]
+    )
 
     .then((result) => {
       return result.rows[0];
     });
 };
 
-
-const fetchCommentsByArticleId= (article_id) =>{
-  
+const fetchCommentsByArticleId = (article_id) => {
   return db
     .query(
-      "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;",
+      'SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;',
       [article_id]
     )
     .then((result) => {
-      if (result.rows.length === 0){
-        return Promise.reject({status: 404, msg: "Not found"});
-      } else{
-      return result.rows;
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: 'Not found' });
+      } else {
+        return result.rows;
       }
-    })
-  }
-    
+    });
+};
 
-  const fetchPostedCommentsByArticleId = (article_id, {username, body}) => {     
-
-  if (!article_id ||!username ||!body) {
+const fetchPostedCommentsByArticleId = (article_id, { username, body }) => {
+  if (!article_id || !username || !body) {
     return Promise.reject({ status: 400, msg: 'Incomplete comment' });
-  } 
-
-  else{   
-    return db.query(`INSERT INTO comments (article_id, author, body) VALUES ($1, $2, $3) RETURNING *;`, [article_id, username, body])
-    .then((result) => { 
-     
-        return result.rows[0]    
-  })
-}
+  } else {
+    return db
+      .query(
+        `INSERT INTO comments (article_id, author, body) VALUES ($1, $2, $3) RETURNING *;`,
+        [article_id, username, body]
+      )
+      .then((result) => {
+        return result.rows[0];
+      });
   }
+};
 
 const fetchVotedArticlesById = (article_id, voteChange) => {
-if (typeof voteChange !== 'number' || voteChange === undefined) {
+  if (typeof voteChange !== 'number' || voteChange === undefined) {
     return Promise.reject({ status: 400, msg: 'Bad request' });
-  }   
-  return db.query(`UPDATE articles 
+  }
+  return db
+    .query(
+      `UPDATE articles 
   SET votes = votes + $2 
-  WHERE article_id = $1 RETURNING * ;`
-  , [article_id, voteChange])
+  WHERE article_id = $1 RETURNING * ;`,
+      [article_id, voteChange]
+    )
 
-  .then((result) => {
-if(result.rowCount === 0){
-  return Promise.reject({status: 404, msg: "Not found"});
-} 
- return result.rows[0];    
-  })
-}
-  
+    .then((result) => {
+      if (result.rowCount === 0) {
+        return Promise.reject({ status: 404, msg: 'Not found' });
+      }
+      return result.rows[0];
+    });
+};
+
 const fetchUsers = () => {
   return db.query(`SELECT * FROM users; `).then((result) => {
-    
     return result.rows;
-  })
-}
-
-
-
+  });
+};
 
 module.exports = {
   fetchTopics,
@@ -133,5 +137,4 @@ module.exports = {
   fetchPostedCommentsByArticleId,
   fetchVotedArticlesById,
   fetchUsers,
-  
 };
